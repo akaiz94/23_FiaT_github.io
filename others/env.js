fd973a56de729e5d6055aacdddd7c9c80a99a59b
AC932352\asdf8 <asdf83244600@gmail.com>
2024-08-31 09:52:44 +0900

    url 추가

---
+++ others/env.js
@@ -0,0 +1,8 @@
+
+// const AmoreCityLab_URL="https://amore-citylab.amorepacific.com:8000/"; // PRD
+
+const AmoreCityLab_URL="https://stg-amore-citylab.amorepacific.com:8000/"; // STG
+const AmoreCityLab_sso_URL="https://stg-amore-citylab.amorepacific.com"; // STG
+
+
+// const AmoreCityLab_URL="http://127.0.0.1:8000/"; // DEV
